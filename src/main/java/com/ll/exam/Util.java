cf1e08d08df86c1755e3fdbdb1e71b9e73a8d164
kimdonghyeon3 <sh2115521@naver.com>
2022-10-29 00:32:09 +0900

    feat : controller scan을 통해서 객체들을 자동으로 만들어 주는 Spring Container 구현 완료

--- src/main/java/com/ll/exam/Util.java
+++ src/main/java/com/ll/exam/Util.java
@@ -1,6 +1,8 @@
 package com.ll.exam;
 
+import com.ll.exam.annotation.Controller;
 import com.ll.exam.article.controller.ArticleController;
+import org.reflections.Reflections;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,8 +15,10 @@ public class Util {
         static {
             objects = new HashMap<>();
 
-            objects.put(ArticleController.class, new ArticleController());
-            objects.put(HomeController.class, new HomeController());
+            Reflections ref = new Reflections("com.ll.exam");
+            for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
+                objects.put(cls, Util.cls.newObj(cls, null));
+            }
         }
 
         public static <T> T getObj(Class<T> cls) {
